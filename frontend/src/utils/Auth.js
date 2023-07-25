681c707223dfcd90049e9a9075bfb5f1f52c5343
ValentinaZam <abc99-33@yandex.ru>
2023-07-25 13:46:48 +0300

    1

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "project-Mesto-deploy.nomoredomains.xyz";
 //"https://auth.nomoreparties.co"
 
 const checkResponse = (res) => {
